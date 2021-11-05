3a912a632b6f06f91645ec68c96729954a32f3cf
Wolf08 <alejandro.montecillo@gmail.com>
2021-11-05 09:50:05 -0600

    Creacion de rutas home y search y creacion de peticiones

--- spotiapp/src/app/app.module.ts
+++ spotiapp/src/app/app.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+//Importar router module 
+import { RouterModule, Routes } from '@angular/router';
+import {HttpClientModule} from '@angular/common/http'
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { SearchComponent } from './components/search/search.component';
 import { ArtistaComponent } from './components/artista/artista.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
+//Importacion de rutas 
+import { ROUTES } from './app.routes';
 
 @NgModule({
   declarations: [
@@ -16,7 +22,11 @@ import { NavbarComponent } from './components/shared/navbar/navbar.component';
     NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    //Module de http
+    HttpClientModule,
+    //Importacion de rutas desde el archivo de routes
+    RouterModule.forRoot(ROUTES,{ useHash: true})
   ],
   providers: [],
   bootstrap: [AppComponent]
--- spotiapp/src/app/components/home/home.component.ts
+++ spotiapp/src/app/components/home/home.component.ts
@@ -1,13 +1,36 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-
+import { SpotifyService } from 'src/app/services/spotify.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  
+  paises: any []=[] ;
+  nuevasCanciones: any []=[] ;
+  
 
-  constructor() { }
+
+  constructor(private http:HttpClient, private spotifyService : SpotifyService ) {
+    
+    //Ejemplo de como usar una peticion http con paises 
+    // console.log("Constructor del log");
+    // this.http.get('https://restcountries.com/v2/lang/es')
+    // .subscribe(  (respuesta:any) =>{
+    //   this.paises = respuesta;
+    //   console.log(respuesta);
+    //   }
+    // );
+
+    //Obtencion de la informacion de los albums para mostrarlos en tarjetas 
+    this.spotifyService.getReleases()
+    .subscribe((data:any) => {
+      console.log(data);
+      this.nuevasCanciones = data.albums.items;
+    });
+  }
 
   ngOnInit(): void {
   }
